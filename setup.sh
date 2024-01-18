e4bff87912f08593d6144844c970f841900f3b9d
flloschy <floschy.email@gmail.com>
2024-01-18 01:10:26 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 mkdir ./data
+mkdir ./data/temp
+mkdir ./data/videos
+
+touch ./data/waitlist.txt
+touch ./data/log.txt
 
 touch ./data/data.json
 echo "{}" > ./data/data.json
@@ -8,9 +13,5 @@ echo "{}" > ./data/data.json
 touch ./data/settings.json
 echo "{\"autoDownloadingEnabled\":false,\"waitListIntervalTime\":1800,\"channelListIntervalTime\":7200,\"systemRoot\":\"C:/\",\"pythonCommand\":\"python3\"}" > ./data/settings.json
 
-touch ./data/waitlist.txt
 touch ./data/latest.txt
-touch ./data/log.txt
-
-mkdir ./data/temp
-mkdir ./data/videos
+echo "0,0" > ./data/latest.txt
