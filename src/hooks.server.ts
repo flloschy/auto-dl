dda9198263b8e8acac7e08de9704ee7be0f5c685
Floschy <69321390+flloschy@users.noreply.github.com>
2024-07-18 19:26:16 +0000

    added logging to hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,21 @@
 import { mkdirSync, readFileSync } from 'fs';
 import { redirect } from '@sveltejs/kit';
+import Logger from '$lib/logger';
+const logger = Logger.getLogger("auth")
 
-const password = readFileSync("./PASSWORD")
-const tryMkDir = (path:string) => {try {mkdirSync(path)} catch {/*empty*/} }
+const password = readFileSync("./PASSWORD").toString()
+const tryMkDir = (path:string) => {
+	logger.debug("Attempting to create", path)
+	try {
+		mkdirSync(path)
+		logger.debug("Created path", path)
+	} catch {
+		logger.debug("Path already exists", path)
+	}
+}
 
-console.log("password: " + password)
+logger.info("The active password is", password)
+
 
 tryMkDir("./downloads")
 tryMkDir("./downloads/music")
@@ -15,16 +26,20 @@ export const handle = async ({ event, resolve }) => {
 	const cookies = event.cookies;
 
 	if (event.url.pathname == '/logout') {
+		logger.debug("Logout route")
 		cookies.delete('session', { path: '/' });
 		throw redirect(301, '/login');
 	}
 	if (event.url.pathname == "/login") {
+		logger.debug("login route")
 		return await resolve(event)
 	}
 
 	const session = cookies.get('session');
 	if (session) {
+		logger.debug("session exists")
 		if (password == session) {
+			logger.debug("Correct passoword")
 			const expires = new Date(Date.now() + 2629800000); // 1 month in MS
 			cookies.set('session', password, {
 				path: '/',
@@ -32,6 +47,9 @@ export const handle = async ({ event, resolve }) => {
 			});
 			return await resolve(event);
 		}
+		logger.debug("Incorrect Password:", session)
 	}
+
+	logger.debug("Route to login")
 	throw redirect(301, "/login")
 };
